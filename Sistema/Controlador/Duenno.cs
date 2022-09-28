bec06db06c78e2669d1c8c20624a30e0795fd629
Andriw03 <66891721+Andriw03@users.noreply.github.com>
2022-09-28 11:38:41 -0300

    Update

--- Sistema/Controlador/Duenno.cs
+++ Sistema/Controlador/Duenno.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,48 @@ using System.Threading.Tasks;
 
 namespace Controlador
 {
-    class Class1
+    class Duenno : Conexion
     {
+        public string RutDuenno { get; set; }
+        public string PrimerNombre { get; set; }
+        public string SegundoNombre { get; set; }
+        public string PrimerApellido { get; set; }
+        public string SegundoApellido { get; set; }
+        public string CorreoElectronico { get; set; }
+        public string Telefono { get; set; }
+
+        public Duenno()
+        {
+            this.Init();
+        }
+        private void Init()
+        {
+            RutDuenno = string.Empty;
+            PrimerNombre = string.Empty;
+            SegundoNombre = string.Empty;
+            PrimerApellido = string.Empty;
+            SegundoApellido = string.Empty;
+            CorreoElectronico = string.Empty;
+            Telefono = string.Empty;
+        }
+
+        public bool BuscarDuenno(string rut)
+        {
+            try
+            {
+                Conectar();
+                cmd = new MySqlCommand("SELECT * FROM Cliente where RutCliente = '" + id + "' ", conex);
+                rd = cmd.ExecuteReader();
+                bool e = rd.Read();
+                rd.Close();
+                return e;
+
+            }
+            catch (Exception ex)
+            {
+                return true;
+
+            }
+        }
     }
 }
