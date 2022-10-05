c9deee57e904fd54d896aeb3bbbed9189f20e9a2
Ignacio Cabrera <icabrera.g17@gmail.com>
2022-10-05 12:29:37 -0300

    Desarrollo v2.2
    correccion datos en moderador soporte

--- Sistema/UniOnline/Moderador/WPF_Moderador_Soporte_Tickets.xaml.cs
+++ Sistema/UniOnline/Moderador/WPF_Moderador_Soporte_Tickets.xaml.cs
@@ -32,7 +32,7 @@ namespace UniOnline.Moderador
             try
             {
                 MySqlConnection con = new MySqlConnection("server=unificacion.cmvnu851mzxa.us-east-1.rds.amazonaws.com;user id=root;password=nohomo123;persistsecurityinfo=True;database=UNIONLINE");
-                MySqlCommand cmd = new MySqlCommand("SELECT * FROM UNIONLINE.FORMULARIO;", con);
+                MySqlCommand cmd = new MySqlCommand("SELECT id_formulario, nombre_form, telefono, correo_form, asunto_form, detalle_form FROM UNIONLINE.FORMULARIO;", con);
                 MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
                 DataTable dt = new DataTable();
                 adapter.Fill(dt);
