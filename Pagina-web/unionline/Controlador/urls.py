c57d28946f583bfde9ea328fe549033e1c79dc92
Sebastian Escobar <83789440+SebaAecssj@users.noreply.github.com>
2022-10-25 19:28:37 -0300

    web v2.5
    Termino de diseño de interfaz de registro de usuario y comienzo de funcionalidad

--- Pagina-web/unionline/Controlador/urls.py
+++ Pagina-web/unionline/Controlador/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from .views import inicio, login
+from .views import inicio, login, crearCuenta
 
 urlpatterns = [
     #Links para redireccionar a otras paginas
     path('', inicio, name='inicio'),
     path('login', login, name='login'),
+    path('registrarse', crearCuenta, name ='registrarse'),
 ]
--- Pagina-web/unionline/Controlador/views.py
+++ Pagina-web/unionline/Controlador/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib import auth
-from . models import Usuario
+from . models import Cbr, Usuario, TUsuario
+from django.contrib.auth.forms import UserCreationForm
 from werkzeug.security import generate_password_hash, check_password_hash
 
 
@@ -22,3 +23,22 @@ def inicio(request):
 
 def login(request):
     return render(request, 'Registration/login.html')
+
+def crearCuenta(request):
+    if request.method == 'POST':
+        usuario = Usuario()
+        cbr = Cbr()
+        tipoU = TUsuario()
+        usuario.rut_usuario = request.POST.get('RUT')
+        usuario.primer_nombre = request.POST.get('Nombre')
+        usuario.segundo_nombre = request.POST.get('Segundo_Nombre')
+        usuario.segundo_nombre = request.POST.get('Primer_Apellido')
+        usuario.segundo_nombre = request.POST.get('Segundo_Apellido')
+        usuario.segundo_nombre = request.POST.get('Telefono')
+        usuario.segundo_nombre = request.POST.get('Correo')
+        usuario.segundo_nombre = request.POST.get('Contraseña')
+        tipoU.id_tipoU = 2
+        cbr.cbr_id_cbr = 1
+
+        usuario.save()
+    return render(request, 'registration/registrar.html')
