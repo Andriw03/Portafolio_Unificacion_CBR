23c8018d09adec7aff390893bb8c8807b225c4af
Estefla <66891851+Estefla@users.noreply.github.com>
2022-10-31 16:52:41 -0300

    Creacion home
    front end home

--- Pagina-web/unionline/Controlador/urls.py
+++ Pagina-web/unionline/Controlador/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
+<<<<<<< Updated upstream
 from .views import inicio, iniciar_sesion, crearCuenta, perfil
+=======
+from .views import inicio, login, crearCuenta, perfil, paginaPrinc
+>>>>>>> Stashed changes
 
 urlpatterns = [
     #Links para redireccionar a otras paginas
@@ -8,5 +12,9 @@ urlpatterns = [
     path('iniciar_sesion', iniciar_sesion, name='iniciar_sesion'),
     path('registrarse', crearCuenta, name ='registrarse'),
     path('perfil', perfil, name ='perfil'),
+<<<<<<< Updated upstream
 
+=======
+    path('home', paginaPrinc, name ='home'),
+>>>>>>> Stashed changes
 ]
