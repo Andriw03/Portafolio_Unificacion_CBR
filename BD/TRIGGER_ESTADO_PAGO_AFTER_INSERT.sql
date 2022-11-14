b3ada6968c19a08654a69029d1395140ba4ae687
Andriw03 <66891721+Andriw03@users.noreply.github.com>
2022-11-14 18:51:31 -0300

    Update

--- BD/TRIGGER_ESTADO_PAGO_AFTER_INSERT.sql
+++ BD/TRIGGER_ESTADO_PAGO_AFTER_INSERT.sql
@@ -1,15 +1,19 @@
+USE `UNIONLINE`;
+
+DELIMITER $$
+
 USE `UNIONLINE`$$
 DROP TRIGGER IF EXISTS `UNIONLINE`.`ESTADO_PAGO_AFTER_INSERT` $$
 USE `UNIONLINE`$$
 CREATE DEFINER = CURRENT_USER TRIGGER `UNIONLINE`.`ESTADO_PAGO_AFTER_INSERT` AFTER INSERT ON `ESTADO_PAGO` FOR EACH ROW
 BEGIN
-	IF FN_AGREGAR_BOLETA (NEW.id_estado) then
-		INSERT INTO  `UNIONLINE`. `B_PAGO` (`fecha_emision`,`monto_pago`,`tipo_pago`,`nombre_conservador`)
-		VALUES
-		(now(),monto_total,tipoP,conservador);
-	ELSE
-		INSERT INTO  `UNIONLINE`. `ERROR` (`codigo_error`,`mensaje_error`,`fecha_error`)
-		VALUES
-		('1002','Carrito de compra sin monto',now());
-	END IF;
+declare var_monto INTEGER; 
+declare tipoP varchar(50);
+SELECT nombre_tipoP into tipoP FROM UNIONLINE.ESTADO_PAGO inner join UNIONLINE.TIPO_PAGO on UNIONLINE.ESTADO_PAGO.TIPO_PAGO_id_tipoP = UNIONLINE.TIPO_PAGO.id_tipoP where id_estado = NEW.id_estado;
+set var_monto = FN_AGREGAR_BOLETA (NEW.id_estado);
+INSERT INTO  `UNIONLINE`. `B_PAGO` (`fecha_emision`,`monto_pago`,`tipo_pago`) VALUES (now(),monto_total,tipoP);
+UPDATE `UNIONLINE`.`CAR_COMPRA` SET `estado` = 1 WHERE `id_carrito` = NEW.CAR_COMPRA_id_carrito;
 END$$
+
+
+DELIMITER ;
